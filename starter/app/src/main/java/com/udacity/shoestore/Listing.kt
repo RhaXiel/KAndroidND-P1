930bcd895962658e7d96873fe206a1dc1829b9f6
RhaXiel <RhaXiel@users.noreply.github.com>
2020-11-25 03:22:42 -0600

    Add viewmodel and linearlayout

--- starter/app/src/main/java/com/udacity/shoestore/Listing.kt
+++ starter/app/src/main/java/com/udacity/shoestore/Listing.kt
@@ -6,18 +6,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListAdapter
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.NavHostFragment
 import com.udacity.shoestore.databinding.FragmentListingBinding
+import com.udacity.shoestore.models.Shoe
+import timber.log.Timber
 
 class Listing : Fragment() {
+    private lateinit var listingViewModel: ListingViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding : FragmentListingBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_listing, container, false)
+        val binding: FragmentListingBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_listing, container, false)
 
+        listingViewModel = ViewModelProvider(this).get(ListingViewModel::class.java)
+
+        binding.listingViewModel = listingViewModel
+        binding.setLifecycleOwner(this)
+
+        listingViewModel.shoesListing.observe(viewLifecycleOwner, Observer { shoesListing ->
+            val shoeNames = arrayOfNulls<String>(shoesListing.size)
+            for (i in 0 until shoesListing.size) {
+                val shoe = shoesListing[i]
+                shoeNames[i] = shoe?.name
+            }
+            //Timber.i(shoeNames.toString())
+            val adapter = ArrayAdapter(
+                requireActivity(),
+                android.R.layout.simple_list_item_1,
+                shoeNames
+            ) //listItems.toList().toString()
+            binding.shoesListview.adapter = adapter
+            //Timber.i(shoesListing.toString())
+        })
 
 
         return binding.root
