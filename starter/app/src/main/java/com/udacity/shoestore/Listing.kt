8ca564bf72290d5f2034274034f09599dee250cd
RhaXiel <RhaXiel@users.noreply.github.com>
2020-12-02 21:58:02 -0600

    list changed

--- starter/app/src/main/java/com/udacity/shoestore/Listing.kt
+++ starter/app/src/main/java/com/udacity/shoestore/Listing.kt
@@ -8,11 +8,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ListAdapter
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.NavHostFragment
 import com.udacity.shoestore.databinding.FragmentListingBinding
 import com.udacity.shoestore.models.Shoe
+import kotlinx.android.synthetic.main.fragment_listing.view.*
 import timber.log.Timber
 
 class Listing : Fragment() {
@@ -37,13 +39,16 @@ class Listing : Fragment() {
                 val shoe = shoesListing[i]
                 shoeNames[i] = shoe?.name
             }
+            //binding.shoesLinearlayout.shoeName_textview.text = shoe?.name
             //Timber.i(shoeNames.toString())
             val adapter = ArrayAdapter(
                 requireActivity(),
                 android.R.layout.simple_list_item_1,
                 shoeNames
             ) //listItems.toList().toString()
-            binding.shoesListview.adapter = adapter
+            //val view = binding.root
+            //setContentView(view)
+            //binding.shoesListview.adapter = adapter
             //Timber.i(shoesListing.toString())
         })
 
